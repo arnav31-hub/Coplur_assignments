8d72c3a4f0fdec2abe730ae2edb6b1f425256d2d
Test <test@Tests-MacBook-Air.local>
2026-06-07 12:38:46 +0530

    Add training program assignments

---
+++ assignment2.py
@@ -0,0 +1,3 @@
+#2)Using input ask user to enter his/her name and output user name with a greeting message:
+name = input("Enter your name")
+print("Welcome," + name + "!It is great to see you today.")
