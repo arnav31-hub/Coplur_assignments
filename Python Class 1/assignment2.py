c194d6e1af3d550ee24b01614134bd540f25e6e8
Test <test@Tests-MacBook-Air.local>
2026-06-13 09:46:07 +0530

    Update assignment solution

--- Python Class 1/assignment2.py
+++
@@ -1,3 +0,0 @@
-#2)Using input ask user to enter his/her name and output user name with a greeting message:
-name = input("Enter your name")
-print("Welcome," + name + "!It is great to see you today.")
